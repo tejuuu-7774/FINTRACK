0186f7c72de4164beccfd3762465f5c753d24ca4
Palwai Tejaswini <palwaitejaswini7777@gmail.com>
2026-04-01 17:15:50 +0530

    Database connection added and tested

---
+++ backend/src/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "../config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("FINTRACK API is running...");
+});
+
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Server failed to start:", error.message);
+  }
+};
+
+startServer();
